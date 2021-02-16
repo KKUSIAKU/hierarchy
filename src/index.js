6f01b18a951f2e467ed03a8da590a9de90ad636c
kkusiaku <kusiaku.k@gmail.com>
2021-02-16 21:45:27 +0100

    first insertion and search implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Node {
   constructor(token) {
@@ -28,6 +29,10 @@ class Node {
     // should also check that the parent has this node as child
     // delete to three
     this.parent = ancestor;
+  }
+
+  isSame(node){
+    return this === node; 
   }
 
   removeChild(node) { 
@@ -65,18 +70,110 @@ class Tree {
     }
   }
 
-  // isRoot() {
-  //   if(!this.root) {
-  //     throw new ReferenceError('Tree object is empty')
-  //   }
-  //   return this.current !== this.root;
-  // }
+  insert(node, parent){
+    if(this.isEmpty()) {
+      throw new ReferenceError('Tree is empty, could not find root node')
+    }
+
+    if(!node || !parent) {
+      throw new TypeError('Insert require exactly two argument: the child node and the parent node in that order')
+    }
+    const checkParentNode = this.find(parent) 
+
+    if(!checkParentNode) {
+      throw new ReferenceError('Parent node is not in the tree')
+    }
+
+    parent.appendChild(node);
+    node.setParent(parent);
+  }
+
+
+  findHelper(node, treeNode) {
+      if(treeNode.isLeaf()){
+        return treeNode === node ? treeNode : null
+      }
+      return treeNode.children.find((n) => n === node);
+  }
 
 
 
-  // insert(parent, node){
-  //   this.size +=1;
-  // }
+  findRecursive(node, treeNode, match=false) {
+      if(node === treeNode) {
+        console.log('oups');
+        // return treeNode
+      }
+
+      if(!treeNode.isLeaf()){
+        for(let child of treeNode.children) {
+          console.log('&&&&&&&&&&', child)
+          return this.findRecursive(node, child)
+        }
+      } else {
+        
+      }
+      
+      return false; 
+  }
+
+  dfsRecurse = function*(node, treeNode){
+    console.log(('&&&&&&&&&&&&&&&&&&&&&&'))
+    let found = false; 
+    if (node.isSame(treeNode)) {
+      return treeNode;
+    }
+
+    if(!treeNode.isLeaf()) {
+      const search = treeNode.children.find((n) => node === n);
+
+      if( search ) {
+        return search
+      }
+
+      const stack = Object.keys(treeNode.children);
+      while(stack.length && !found) {
+      const next = treeNode.children[stack.shift()];
+       found = yield* this.dfsRecurse(node, next)
+      }
+
+      if(found) {
+        console.log('""""""""""""""""""""""""')
+        return found;
+      }
+    }
+
+
+    return found;
+  }
+
+  // start stree from Node(tree) children
+  // method should be accessible normale from outside, not enumerable
+  dfs(node, tree) {
+    let queue = [];
+    queue.push(...tree.children)
+    let target = null;
+    while (queue.length && !target) {
+      const current = queue.shift();
+      if( current == node ) {
+        target = current
+      } else if ( current.children) {
+        const newQueu = [];
+        newQueu.push(...current.children)
+        queue = newQueu.concat(queue);
+      }
+    }
+    return target;
+  }
+
+  
+  // isSame method
+  find(node) {
+    if(node === this.root) {
+      return this.root
+    }
+    const targetNode = this.dfs(node, this.root)
+    return  targetNode
+  }
 
 }
 
--- test/tree.test.js
+++ test/tree.test.js
@@ -25,29 +25,90 @@ describe('', () => {
     }).to.throw();
   })
 
-  it('Should set twice root node on the tree', (done) => {
+  it('should throw if tree is empty', (done) => {
+    const t = new Tree(); 
+    const n1 = new Node(); 
+    const n2 = new Node();
+    expect(() => {
+      t.insert(n1, n2)
+    }).to.throw()
+    done()
+  })
+
+  it('should throw if parent node is not provided', (done) => {
     const t = new Tree();
-    //   const root = new Node();
-    //   t.setRoot(root);
-    //   expect(t.root).to.deep.equal(root)
-    // })
+    const r = new Node();
+    t.setRoot(r);
+    const n1 = new Node(); 
+    expect(() => {
+      t.insert(n1)
+    }).to.throw()
+    done()
+  })
 
+  it('should throw if parent node is not in the tree', (done) => {
+    const r1 = new Node('r1');
+    const r1_n1 = new Node('r1_n1')
+    const r1_n2 = new Node('r1_n2')
+    const r1_n3 = new Node('r1_n3')
+    const r1_n11 = new Node('r1_n11')
+    const r1_n12 = new Node('r1_n12')
+    r1_n1.appendChild(r1_n11);
+    r1_n1.appendChild(r1_n12);
+    r1.appendChild(r1_n1)
+    r1.appendChild(r1_n2);
+    r1.appendChild(r1_n3);
+    const t = new Tree();
+    t.setRoot(r1)
 
-    // it('Should have size 1', () => {
-    //   const t = new Tree();
-    //   const root = new Node();
-    //   t.setRoot(root);
-    //   expect(t.size).to.equal(1)
-    // })
+    const r1_n4 = new Node('r1_n4')
+    const new_node = new Node('new');
 
+    expect(() => {
+      t.insert(new_node, r1_n4)
+    }).to.throw()
+    done()
+  })
+// need to test more insertion scenario or case
+  it('Should insert node on internal node', (done) => {
+    const r1 = new Node('r1');
+    const r1_n1 = new Node('r1_n1')
+    const r1_n2 = new Node('r1_n2')
+    const r1_n3 = new Node('r1_n3')
+    const r1_n11 = new Node('r1_n11')
+    const r1_n12 = new Node('r1_n12')
+    r1_n1.appendChild(r1_n11);
+    r1_n1.appendChild(r1_n12);
+    r1.appendChild(r1_n1)
+    r1.appendChild(r1_n2);
+    r1.appendChild(r1_n3);
 
-    // it('Should set twice root node on the tree', () => {
-    //   const t = new Tree();
-    //   expect(() => {
-    //     t.setRoot()
-    //     t.setRoot()
-    //   }).to.throw()
-    // })
+    const t = new Tree();
+    t.setRoot(r1)
+    const r1_n4 = new Node('r1_n4')
+    expect(r1_n12.children.length).to.equal(0)
+    t.insert(r1_n4, r1_n12);
+    expect(r1_n12.children.length).to.equal(1)
+    expect(r1_n12.children[0]).to.deep.equal(r1_n4)
+    done()
+  })
+
+  it('Should find node leaf node', (done) => {
+    const r1 = new Node('r1');
+    const r1_n1 = new Node('r1_n1')
+    const r1_n2 = new Node('r1_n2')
+    const r1_n3 = new Node('r1_n3')
+    const r1_n11 = new Node('r1_n11')
+    const r1_n12 = new Node('r1_n12')
+    r1_n1.appendChild(r1_n11);
+    r1_n1.appendChild(r1_n12);
+    r1.appendChild(r1_n1)
+    r1.appendChild(r1_n2);
+    r1.appendChild(r1_n3);
+
+    const t = new Tree();
+    t.setRoot(r1)
+    expect(t.find(r1_n11)).to.deep.equal(r1_n11)
     done()
   })
 })
